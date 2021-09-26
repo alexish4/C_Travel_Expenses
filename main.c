42aae2785115d92492e69397a98a31aff838b3f6
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2021-09-25 22:04:45 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "header.h"
+
+int main()
+{
+    printf("-------------------Travel Expenses-------------------\n
+            Please enter the number of days you will be traveling");
+
+}
