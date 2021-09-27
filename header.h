fb45e3166dc57d5495f24aa527fc487b3b2d92c4
Alex Hernandez <alexish4@cpp.edu>
2021-09-26 18:12:36 -0700

    Updated with double variables

--- header.h
+++ header.h
@@ -2,40 +2,51 @@
 #define HEADER_H
 
 //variables
+double airFare, carRental, parkingF, taxiF, regisF, parkingTotal, hotelF, taxiTotal;
 int days, 
     timeDeparted, //24 hr time?
     timeArrivedHome,
-    airFare, // air fare total
-    carRental, //car rental total
     miles, //if private car used
     carExpense,
-    parkingF,
-    taxiF,
-    parkingTotal, // total amount person spent
-    regisF, // is this a set value???
-    hotelF,
+    taxiTotal,
     meals;
 int breakfast = 9;
 int lunch = 12;
 int dinner = 16;
 
-// returning info
+//Geting information from user
 /*NOTE ON INPUT VALIDATION:
   - no neg numbers for dollar amount, or miles driven
   - no values less than 1 for number of days
   - only valid times (24 hour time?)*/
-int daysOnTrip(); // asking how many days spent
-int timeDeparted_Arrive(); //departure info for first day and arrival home last day
+int getDaysOnTrip(); // asking how many days spent
+int getTimeDeparture(); //departure info for first day and arrival home last day
+int getTimeArriveHome();
+double getAirFareCost();
+double getCarRentalCost();
+int getMilesDriven(); // if private car used, ask how many miles.
+double getParkingFees();
+double getTaxiFees();
+double getMeetingFees();
+double getHotelFees();
+double getMealCostsTotal()
+double inputValidMoney(double amt)
 
-int milesDriven(); // if private car used, ask how many miles.
-//is rental == car rental????
+
 int regisFee();
 
-// calulations
+// car calulations
 int privCarExpense(int miles); // Cost is $0.27 per mile
-int parkingFee(int days, int parkingTotal); // allows $6 per day, anything more must be covered by employee
-int taxiFee(int days, int taxiTotal); // allows $10 per day
-int hotelFee(int days); // allows up to $90 per night
+double parkingFees(int days, double parkingTotal); // allows $6 per day, anything more must be covered by employee
+double privCarExpense(int miles);
+double taxiFees(int days, double taxiTotal); // allows $10 per day
+//hotel and meal calculations
+double meetingFees();
+double hotelFee(int days, int hotelTotal); // allows up to $90 per night
+int breakfastsEaten(int days, int timeArrivedHome, int timeDeparted);
+int lunchesEaten(int days, int timeArrivedHome, int timeDeparted);
+int dinnersEaten(int days, int timeArrivedHome, int timeDeparted);
+double mealCost(int breakfasts, int lunches, int dinners, double expenditure);
 /* FIRST DAY:
    Breakfast allowed if time of departure is before 07:00
    Lunch is allowed if time of departure is before 12:00
@@ -46,6 +57,8 @@ int hotelFee(int days); // allows up to $90 per night
    Lunch is allowed if the time of arrival is after 13:00 
    Dinner is allowed on the last day if the time of arrival is after 19:00
 */
+double hotelFees();
+double mealCost(int breakfasts, int lunches, int dinners, double expenditure);
 int mealEaten(int days, int timeArrivedHome, int timeDeparted);
 
 
