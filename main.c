2dbcdb8c483db7abb1a27531f71b57e3e140b799
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2021-09-26 20:59:35 -0700

    updates on total and parking/taxi fees

--- main.c
+++ main.c
@@ -6,19 +6,33 @@ int main()
     double total = 0;
     printf("Travel Expenses Calculator");
     days = getDaysOnTrip();
+    timeDeparted = getTimeDeparture();
+    timeArrivedHome = getTimeArriveHome();
+    // car fees
     miles = getMilesDriven();
-    taxiF = getTaxiFees();
-    parkingF = getParkingFees();
-    getTimeDeparture();
-    getTimeArriveHome();
-    getAirFareCost();
-    getCarRentalCost();
+    taxiTotal = getTaxiFees();
+    parkingTotal = getParkingFees();
+    carRental = getCarRentalCost();
+    // hotel/ meeting fees
+    airFare = getAirFareCost();
+    regisF = getMeetingFees();
+    hotelF = getHotelFees();
+    // meal fees
+    meals = getMealCostsTotal(); // from user
+    //calculating company coverage
+    breakfast = breakfastsEaten(days, timeArrivedHome, timeDeparted);
+    lunch = lunchesEaten(days, timeArrivedHome, timeDeparted);
+    dinner = dinnersEaten(days, timeArrivedHome, timeDeparted);
 
-    //getParkingFees();
-
-    printf("The total expenses for this trip: $lf\n", total);
-    total = parkingFees(days, parkingF) + 
-            privCarExpense(miles) + taxiFees(days, taxiF);
+    
+            //car fees
+    total = parkingFees(days, parkingTotal) + 
+            privCarExpense(miles) + 
+            taxiFees(days, taxiTotal) +
+            // hotel and meal fees
+            hotelFee(days, hotelF)+
+            mealCost(breakfast, lunch, dinner, meals);
+    printf("The total expenses for this trip: $%lf\n", total);
     printf("The total allowable expenses for this trip: \n");
     printf("The total amount to be reimbursed: \n");
     printf("Amount saved: \n");
